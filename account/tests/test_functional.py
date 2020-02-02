60851ecd1b87fd5ce6f8a725f16dff30752acee7
CaroleSa <carole220287@hotmail.com>
2020-02-02 14:21:44 +0100

    account app - test_functional.py : add test_access_result_page

--- account/tests/test_functional.py
+++ account/tests/test_functional.py
@@ -4,19 +4,41 @@
 """ TestUserTakesTheTest class """
 
 # imports
-from unittest import TestCase
 from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.keys import Keys
+import time
 
 
-class TestUserTakesTheTest(TestCase):
+class TestUserTakesTheTest(StaticLiveServerTestCase):
     """ geckodriver and firefox
     must be in your path """
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
-
-    def test_results_page_shows(self):
-        self.driver.get("http://206.189.30.225/")
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
-        self.driver.close()
+        self.browser.quit()
+
+    def test_access_result_page(self):
+        # set url
+        self.browser.get(self.live_server_url + "/")
+        # get module select
+        elem = self.browser.find_element_by_id('searchTextarea')
+        elem.send_keys("nutella")
+        elem.send_keys(Keys.RETURN)
+        time.sleep(5)
+        self.assertEqual(self.browser.current_url, self.live_server_url + "/food/result/")
+
+
+
+    """def test_project_info_form(self):
+        # set url
+        self.browser.get(self.live_server_url + '/')
+        # get module select
+        self.browser.find_element_by_id('userLogoLi').click()
+        test = self.browser.find_element_by_xpath(
+            "//label[@id='mail']"
+        ).click()
+        test.send_keys("test")"""
