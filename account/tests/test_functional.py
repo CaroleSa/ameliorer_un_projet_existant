90414c6773492129f5d020be5b7a8418f0f5d1ee
CaroleSa <carole220287@hotmail.com>
2020-02-03 13:03:06 +0100

    functional tests checked with pylint ok

--- account/tests/test_functional.py
+++ account/tests/test_functional.py
@@ -4,26 +4,33 @@
 """ TestUserTakesTheTest class """
 
 # imports
-from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.keys import Keys
 import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class TestUserTakesTheTest(StaticLiveServerTestCase):
+    """ class TestUserTakesTheTest
+    method test_user_account """
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(10)
+        self.dict_data_access_account = {"mail": "carole@test.fr", "password": "00000000"}
+        self.dict_data_create_account = {"mail": "carole@test.fr", "password": "00000000",
+                                         "password2": "00000000"}
+        self.mail_connection = self.dict_data_access_account.get("mail")
 
     def tearDown(self):
         self.browser.quit()
 
     def test_user_account(self):
+        """ test the functionality of the user account """
         # set url
         self.browser.get(self.live_server_url + "/")
 
-        # test access favorites if no connected
+        # test access favorites if the user is not logged
         self.browser.find_element_by_id("carrotLogoLi").click()
         time.sleep(5)
         # check the url of the recovered page
@@ -32,15 +39,15 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         error_message = self.browser.find_element_by_id("red").text
         self.assertEqual(error_message, "Veuillez vous connecter pour accéder à vos favoris.")
 
-        # test click to account logo
+        # test click to the account logo
         self.browser.find_element_by_id("userLogoLi").click()
         time.sleep(5)
         # check the url of the recovered page
-        self.assertEqual(self.browser.current_url, self.live_server_url + "/account/access_account/")
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + "/account/access_account/")
 
-        # test access account with a mail unknown
-        dict = {"mail": "carole@test.fr", "password": "00000000"}
-        for key, value in dict.items():
+        # connection test with an unknown email
+        for key, value in self.dict_data_access_account.items():
             self.browser.find_element_by_id(key).send_keys(value)
         self.browser.find_element_by_id("submit").click()
         time.sleep(5)
@@ -48,34 +55,36 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         error_message = self.browser.find_element_by_id("red").text
         self.assertEqual(error_message, "Ce compte n'existe pas.")
 
-        # test create account
+        # test of the creation of the user account
         self.browser.find_element_by_id("buttonCreateAccount").click()
-        dict = {"mail": "carole@test.fr", "password": "00000000", "password2": "00000000"}
-        for key, value in dict.items():
+        for key, value in self.dict_data_create_account.items():
             self.browser.find_element_by_id(key).send_keys(value)
         self.browser.find_element_by_id("submitButton").click()
         time.sleep(5)
         # check the url of the recovered page
-        self.assertEqual(self.browser.current_url, self.live_server_url + "/account/create_account/")
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + "/account/create_account/")
         # check the value of the confirmation message
         confirmation_message = self.browser.find_element_by_id("confirmationMessage").text
-        self.assertEqual(confirmation_message, "Le compte carole@test.fr a bien été créé.")
+        self.assertEqual(confirmation_message,
+                         "Le compte " + self.mail_connection + " a bien été créé.")
 
-        # test connection to the user's account with the account created
+        # test of connection with created user
         self.browser.find_element_by_id("userLogoLi").click()
         time.sleep(5)
-        dict = {"mail": "carole@test.fr", "password": "00000000"}
-        for key, value in dict.items():
+        for key, value in self.dict_data_access_account.items():
             self.browser.find_element_by_id(key).send_keys(value)
         self.browser.find_element_by_id("submit").click()
         time.sleep(5)
         # check the url of the recovered page
-        self.assertEqual(self.browser.current_url, self.live_server_url + "/account/access_account/")
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + "/account/access_account/")
         # check the value of the confirmation message
         confirmation_message = self.browser.find_element_by_id("confirmationMessage").text
-        self.assertEqual(confirmation_message, "Bonjour carole@test.fr ! Vous êtes bien connecté.")
+        self.assertEqual(confirmation_message,
+                         "Bonjour " + self.mail_connection + " ! Vous êtes bien connecté.")
 
-        # test access favorites if connected
+        # test access favorites if the user is logged
         self.browser.find_element_by_id("carrotLogoLi").click()
         time.sleep(5)
         # check the url of the recovered page
@@ -84,16 +93,16 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         title = self.browser.find_element_by_id("favoritesTitle").text
         self.assertEqual(title, "Mes aliments")
 
-        # test add favorites food
-        elem = self.browser.find_element_by_id("searchTextarea")
-        elem.send_keys("nutella")
-        elem.send_keys(Keys.RETURN)
+        # test add favorite food
+        search_textarea = self.browser.find_element_by_id("searchTextarea")
+        search_textarea.send_keys("nutella")
+        search_textarea.send_keys(Keys.RETURN)
         time.sleep(5)
         name_food_result_page = self.browser.find_element_by_id("nameFood").text
         self.browser.find_element_by_id("floppy").click()
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/food/result/")
-        # check the favorite food is registered
+        # check that the selected food is present in the favorites page
         self.browser.find_element_by_id("carrotLogoLi").click()
         time.sleep(5)
         name_favorite_food = self.browser.find_element_by_id("nameFood").text
@@ -104,11 +113,11 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         time.sleep(5)
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/food/favorites/")
-        # check the favorite food is deleted
+        # check that the deleted favorite food is not present in the favorites page
         name_favorite_food = self.browser.find_element_by_id("nameFood").text
         self.assertEqual(name_favorite_food, "")
 
-        # test disconnection
+        # test user disconnection
         self.browser.find_element_by_id("exitLogoLi").click()
         time.sleep(5)
         # check the url of the recovered page
@@ -120,8 +129,7 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         # test access to my account page
         self.browser.find_element_by_id("userLogoLi").click()
         time.sleep(5)
-        dict = {"mail": "carole@test.fr", "password": "00000000"}
-        for key, value in dict.items():
+        for key, value in self.dict_data_access_account.items():
             self.browser.find_element_by_id(key).send_keys(value)
         self.browser.find_element_by_id("submit").click()
         time.sleep(5)
@@ -132,12 +140,11 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         # check the value of the title of the my account page
         title = self.browser.find_element_by_id("titleMyAccount").text
         self.assertEqual(title, "Mes informations :")
-        # check the email of the my account page
+        # check that the email of the my account page is the same that connection email
         mail_account = self.browser.find_element_by_id("mailAccount").text
-        mail_connection = dict.get("mail")
-        self.assertEqual(mail_account, "Votre adresse e-mail : " + mail_connection)
+        self.assertEqual(mail_account, "Votre adresse e-mail : " + self.mail_connection)
 
-        # test delete account
+        # test delete user account
         self.browser.find_element_by_id("deleteAccount").click()
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/")
@@ -145,11 +152,10 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         confirmation_message = self.browser.find_element_by_id("confirmationMessage").text
         self.assertEqual(confirmation_message, "Votre compte a bien été supprimé.")
 
-        # test connection with the account deleted
+        # test of connection with the user account deleted
         self.browser.find_element_by_id("userLogoLi").click()
         time.sleep(5)
-        dict = {"mail": "carole@test.fr", "password": "00000000"}
-        for key, value in dict.items():
+        for key, value in self.dict_data_access_account.items():
             self.browser.find_element_by_id(key).send_keys(value)
         self.browser.find_element_by_id("submit").click()
         time.sleep(5)
--- food/tests/test_functional.py
+++ food/tests/test_functional.py
@@ -4,13 +4,15 @@
 """ TestUserTakesTheTest class """
 
 # imports
-from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.keys import Keys
 import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class TestUserTakesTheTest(StaticLiveServerTestCase):
+    """ class TestUserTakesTheTest :
+    method test_search_food """
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,40 +22,48 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         self.browser.quit()
 
     def test_search_food(self):
+        """ test the functionality of the user food searches """
         # set url
         self.browser.get(self.live_server_url + "/")
 
         # test search nutella in textarea (at the top of the page)
-        elem = self.browser.find_element_by_id("searchTextarea")
-        elem.send_keys("nutella")
-        elem.send_keys(Keys.RETURN)
+        search_textarea = self.browser.find_element_by_id("searchTextarea")
+        name_food = "nutella"
+        search_textarea.send_keys(name_food)
+        search_textarea.send_keys(Keys.RETURN)
         time.sleep(5)
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/food/result/")
+        # check the value of the title of the result page
+        title = self.browser.find_element_by_id("titleFoodSearched").text
+        self.assertEqual(title, "Aliment recherché : " + name_food)
+        # check that the result display
+        name_food = self.browser.find_element_by_id("nameFood").text
+        self.assertNotEqual(name_food, "")
 
         # test click on the food image of the result page
         name_food_result_page = self.browser.find_element_by_id("nameFood").text
-        elem = self.browser.find_element_by_id("image")
-        elem.click()
+        food_image = self.browser.find_element_by_id("image")
+        food_image.click()
         time.sleep(5)
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/food/detail/")
 
-        # test that the detailed food (detail page) is the same as the food clicked in the result page
+        # test that the detailed food (detail page) is the same as the food clicked
+        # in the result page
         name_food_detail_page = self.browser.find_element_by_id("nameFoodDetail").text
         self.assertEqual(name_food_result_page, name_food_detail_page)
-        # tester le click sur l'enregistrement et voir si enregistré dans favoris
 
         # test click to the OpenFoodFacts link
-        elem = self.browser.find_element_by_id("link")
-        link = elem.get_attribute("href")
-        elem.click()
+        link = self.browser.find_element_by_id("link")
+        value_link = link.get_attribute("href")
+        link.click()
         # check the OpenFoodFacts link value
-        self.assertRegex(link, "^https://fr.openfoodfacts.org/produit/[0-9]*/[a-z-]*")
+        self.assertRegex(value_link, "^https://fr.openfoodfacts.org/produit/[0-9]*/[a-z-]*")
 
-        # set url
-        self.browser.get(self.live_server_url + "/")
         # test search pizza in textarea (at the middle of the page)
+        self.browser.find_element_by_id("exitLogoLi").click()
+        time.sleep(5)
         self.browser.find_element_by_id("productTextarea").send_keys("pizza")
         self.browser.find_element_by_id("button_search").click()
         time.sleep(5)
@@ -64,10 +74,22 @@ class TestUserTakesTheTest(StaticLiveServerTestCase):
         self.browser.find_element_by_id("floppy").click()
         time.sleep(5)
         # check the url of the recovered page
-        self.assertEqual(self.browser.current_url, self.live_server_url + "/account/access_account/")
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + "/account/access_account/")
 
         # test click to the exit logo
         self.browser.find_element_by_id("exitLogoLi").click()
         time.sleep(5)
         # check the url of the recovered page
         self.assertEqual(self.browser.current_url, self.live_server_url + "/")
+
+        # test a search without result in textarea
+        food_name = "salade"
+        self.browser.find_element_by_id("productTextarea").send_keys(food_name)
+        self.browser.find_element_by_id("button_search").click()
+        time.sleep(5)
+        # check the url of the recovered page
+        self.assertEqual(self.browser.current_url, self.live_server_url + "/food/result/")
+        # check the value of the confirmation message
+        confirmation_message = self.browser.find_element_by_id("confirmationMessage").text
+        self.assertEqual(confirmation_message, "Pas de résultat pour l'aliment " + food_name + ".")
